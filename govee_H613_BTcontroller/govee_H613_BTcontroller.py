031a43632f9d2f27f6dee9aa6d1e1b71da05d23d
Peter Minerba <54323615+Xample33@users.noreply.github.com>
2023-11-23 17:13:23 +0100

    various small fix

--- govee_H613_BTcontroller/govee_H613_BTcontroller.py
+++ govee_H613_BTcontroller/govee_H613_BTcontroller.py
@@ -2,11 +2,13 @@ import asyncio
 import logging
 
 from bleak import BleakClient
-from bleak.exc import BleakDeviceNotFoundError
+from bleak.exc import BleakDeviceNotFoundError, BleakError
 
 from .constants import Constants
-from .utils import calculate_string, convert_color, is_valid_color, split_string, to_bytes
+from .utils import convert_color, is_valid_color, to_bytes, make_string
 
+
+_LOGGER = logging.getLogger(__name__)
 
 class GoveeController:
     def __init__(self, mac_address):
@@ -16,6 +18,11 @@ class GoveeController:
         self.mac_address = mac_address
         self.client = None
         self.name = None
+        
+        self.rgb = None
+        self.color_name = None
+        self.brightness = None        
+        self.power = None
 
     async def connect(self) -> "GoveeController":
         if not self.client:
@@ -27,9 +34,10 @@ class GoveeController:
 
             raw_name = await self.client.read_gatt_char(Constants.GET_NAME_UUID)
             self.name = raw_name.decode("utf-8")
-
+            
+            _LOGGER.debug(f'Connected to {self.name} ({self.mac_address})')
             return self
-
+        
     async def disconnect(self):
         if self.client:
             await self.client.disconnect()
@@ -37,41 +45,49 @@ class GoveeController:
 
     async def get_power(self):
         byte_array = bytearray(to_bytes(Constants.POWER_STATUS))
-        await _get_status(self.device, byte_array)
+        await self._get_status(byte_array)
+        return self.power
 
     async def get_rgb(self):
         byte_array = bytearray(to_bytes(Constants.RGB_STATUS))
-        await _get_status(self.device, byte_array)
+        await self._get_status(byte_array)
+        return self.rgb
 
     async def get_brightness(self):
         byte_array = bytearray(to_bytes(Constants.BRIGHTNESS_STATUS))
-        await _get_status(self.device, byte_array)
+        await self._get_status(byte_array)
+        return self.brightness
 
     async def turn_on(self, smooth=False):
+        _LOGGER.debug('Turning on')
         if not isinstance(smooth, bool):
             raise TypeError("smooth argument must be a boolean")
 
         if smooth:
-            for i in range(0, 255, 25):
+            for i in range(0, 255, 50):
                 await self.set_brightness(i)
                 await asyncio.sleep(0.15)
 
         command_bytes = to_bytes(Constants.TURN_ON_COMMAND)
-        await _send_command(self.client, command_bytes)
+        
+        await self._send_command(command_bytes)
 
     async def turn_off(self, smooth=False):
+        _LOGGER.debug('Turning off')
         if not isinstance(smooth, bool):
             raise TypeError("smooth argument must be a boolean")
 
         if smooth:
-            for i in range(255, 0, -25):
+            for i in range(255, 0, -50):
                 await self.set_brightness(i)
                 await asyncio.sleep(0.15)
 
         command_bytes = to_bytes(Constants.TURN_OFF_COMMAND)
-        await _send_command(self.client, command_bytes)
+        
+        await self._send_command(command_bytes)
 
     async def set_brightness(self, brightness):
+        _LOGGER.debug('Setting brightness')
         if not isinstance(brightness, int):
             raise TypeError("Brightness must be an integer")
 
@@ -84,12 +100,14 @@ class GoveeController:
         # Calculate checksum
         checksum = hex(brightness ^ 0x33 ^ 0x04)[2:].zfill(2)
 
-        string = f'3304{brightness_hex}00000000000000000000000000000000{checksum}'.upper()
+        string = f'3304{brightness_hex}00000000000000000000000000000000{checksum}'
 
         # Send the command
-        await _send_command(self.client, to_bytes(string))
+        await self._send_command(to_bytes(string))
 
     async def set_color(self, color):
+        _LOGGER.debug('Setting color')
+        
         if isinstance(color, str):
             # Convert color name to RGB
             if is_valid_color(color_name=color) is False:
@@ -101,47 +119,60 @@ class GoveeController:
         else:
             raise ValueError("Invalid color format, color argument must be tuple or str.")
 
-        # Calculate the command string
-        command_string = calculate_string(rgb)
-
         # Split the command string into bytes
-        command_bytes = split_string(command_string)
-
+        command_bytes = make_string(rgb)
+        
         # Send the command
-        await _send_command(self.client, command_bytes)
+        await self._send_command(to_bytes(command_bytes))
 
-async def _send_command(device, value):
-    logging.debug(f'Sending: {value.hex()}')
-    await device.write_gatt_char(Constants.WRITE_CHARACTERISTIC_UUID, bytearray(value), True)
+    async def _send_command(self, value):
+        _LOGGER.debug(f'Sending: {bytearray(value).hex()}')
+        try:
+            await self.client.write_gatt_char(Constants.WRITE_CHARACTERISTIC_UUID, bytearray(value))
+            await asyncio.sleep(Constants.COMMAND_DELAY)
+        except BleakError as e:
+            raise e
+        
+    async def _notification_handler(self, raw, data: bytearray) -> None:
+        _LOGGER.debug(f"Received: {data.hex()}")
+        
+        if data.hex()[0:4] == 'aa01':
+            self.power = int(data.hex()[4:6], 16)
+            
+            _LOGGER.debug(f'Power: {self.power}')
 
-def _notification_handler(raw, data: bytearray) -> None:
-    logging.debug(f"Raw: {raw}")
-    logging.debug(f"Received: {data.hex()}")
+        elif data.hex()[0:4] == 'aa05':
+            r = int(data.hex()[6:8], 16)
+            g = int(data.hex()[8:10], 16)
+            b = int(data.hex()[10:12], 16)
 
-    if data.hex()[0:4] == 'aa01':
-        power = int(data.hex()[4:6], 16)
-        logging.debug(f'Power: {power}')
+            self.rgb = (r, g, b)
+            self.color_name = convert_color(rgb_tuple=self.rgb)
+            
+            _LOGGER.debug(f'RGB: {self.rgb} ({self.color_name})')
 
-    elif data.hex()[0:4] == 'aa05':
-        r = int(data.hex()[6:8], 16)
-        g = int(data.hex()[8:10], 16)
-        b = int(data.hex()[10:12], 16)
+        elif data.hex()[0:4] == 'aa04':
+            self.brightness = int(data.hex()[4:6], 16)
+            
+            _LOGGER.debug(f'Brightness: {self.brightness}')
+            
+        else:
+            _LOGGER.debug(f'Unknown command: {data.hex()}')
 
-        rgb = (r, g, b)
-        logging.debug(f'RGB: {rgb} ({convert_color(rgb)})')
+    async def _get_status(self, byte_array):
+        # Subscribe to notifications
+        _LOGGER.debug(f'Subscribing to {Constants.READ_CHARACTERISTIC_UUID}')
+        await self.client.start_notify(Constants.READ_CHARACTERISTIC_UUID, self._notification_handler)
+        await asyncio.sleep(Constants.COMMAND_DELAY)
+        
+        # Read the state
+        _LOGGER.debug(f'Sending: {byte_array.hex()}')
+        await self.client.write_gatt_char(Constants.WRITE_CHARACTERISTIC_UUID, byte_array)
+        await asyncio.sleep(Constants.COMMAND_DELAY)
 
-    elif data.hex()[0:4] == 'aa04':
-        brightness = int(data.hex()[4:6], 16)
-        logging.debug(f'Brightness: {brightness}')
-
-async def _get_status(device, byte_array):
-    # Subscribe to notifications
-    logging.debug(f'Subscribing to {Constants.READ_CHARACTERISTIC_UUID}')
-    await device.start_notify(Constants.READ_CHARACTERISTIC_UUID, _notification_handler)
-
-    # Read the state
-    await device.write_gatt_char(Constants.WRITE_CHARACTERISTIC_UUID, byte_array)
-    await asyncio.sleep(Constants.COMMAND_DELAY)
-
-    # Unsubscribe from notifications
-    await device.stop_notify(Constants.READ_CHARACTERISTIC_UUID)
+        # Unsubscribe from notifications
+        _LOGGER.debug(f'Unsubscribing from {Constants.READ_CHARACTERISTIC_UUID}')
+        try:
+            await self.client.stop_notify(Constants.READ_CHARACTERISTIC_UUID)
+        except BleakError("Not connected"):
+            raise BleakError("Not connected")
